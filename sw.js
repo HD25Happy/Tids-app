1ab188b68e1aeab7fe99cc332d8d5ea110bbbe16
HD25Happy <117777229+HD25Happy@users.noreply.github.com>
2026-05-09 17:48:42 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', function(e) {
-  e.respondWith(fetch(e.request).catch(() => new Response('Offline')));
-});
